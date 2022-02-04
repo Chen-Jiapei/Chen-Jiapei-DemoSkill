98e95915edce18f5697f05a1286e994506f9a07f
DisabledRightHand <jic038@ucsd.edu>
2022-02-04 03:10:01 -0800

    Update SkillDemoTest.java

--- SkillDemoTest.java
+++ SkillDemoTest.java
@@ -2,6 +2,7 @@ import static org.junit.Assert.*;
 import org.junit.*; //importing junit test
 
 public class SkillDemoTest {
+    @Test
     public void SkillDemoTest1() {
         assertEquals(true, SkillDemo.isGreater(2, 1));
     }
