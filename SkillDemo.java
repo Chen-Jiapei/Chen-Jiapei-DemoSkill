00ad699ce6769279bad356be37485655538b8c64
DisabledRightHand <jic038@ucsd.edu>
2022-02-04 03:01:19 -0800

    Update SkillDemo

--- SkillDemo.java
+++ SkillDemo.java
@@ -1,1 +1,8 @@
-
+public class SkillDemo {
+    public boolean isGreater(int a, int b) {
+        if(a > b) {
+            return true;
+        }
+        return false;
+    }
+}
---
+++ SkillDemoTest.java
@@ -0,0 +1,9 @@
+import static org.junit.Assert.*;
+import org.junit.*; //importing junit test
+
+public class SkillDemoTest {
+    public void SkillDemoTest1() {
+        SkillDemo Demo = new SkillDemo();
+        assertEquals(false, Demo.isGreater(2, 1));
+    }
+}
